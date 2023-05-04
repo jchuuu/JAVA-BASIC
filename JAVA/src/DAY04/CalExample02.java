8d90ae04e1b26cc45cc3c7c4a91930f709aca824
jchuuu <chu6262j@gmail.com>
2023-05-04 21:40:51 +0900

    DAY04
    증감연산

--- JAVA/src/DAY04/CalExample02.java
+++ JAVA/src/DAY04/CalExample02.java
@@ -24,6 +24,24 @@ public class CalExample02 {
 		System.out.println(x);
 		
 
+		// 증감연산
+		int y = 1;
+		y++; // y = y+1
+		System.out.println("y = "+y);
+		y--; // y = y-1
+		System.out.println("y = "+y);
+		++y;
+		--y;
+				
+		// 증감연산이 앞에 있거나 뒤에 있거나 값은 같음
+		// 프로그램 실행 순서에는 차이가 있음
+		int z = 10;
+		
+		// 선&후 수식에 따라 나타나는 차이점
+		//System.out.println("z = " + (++z)); =====> 11
+		System.out.println("z = " + (z++)); // ====> 10
+		System.out.println(z); // =================> 11
+		
 	}
 
 }
