e2875040c7aea930148a349069e34c749320a00c
jchuuu <chu6262j@gmail.com>
2023-05-16 21:31:20 +0900

    DAY08
    ArrayCopy

---
+++ JAVA/src/DAY08/ArrayCopyExample.java
@@ -0,0 +1,46 @@
+package DAY08;
+
+import java.util.Arrays;
+
+public class ArrayCopyExample { //p133
+
+	public static void main(String[] args) {
+		
+		// *** java 17 api documentation ***
+		
+		// 배열 복사가 없을 때;
+		int a[] = {1,2};
+		int b[] = a;
+		
+		b[1] = 100;
+		System.out.println("a[1] : " + a[1]);
+		
+		// 1. for문을 활용한 복사
+		int a1[] = {1,2};
+		int b1[] = new int[a1.length];
+		
+		for(int x=0; x<a1.length; x++) {
+			b1[x] = a1[x];
+		}
+		
+		b1[1] = -1000;
+		System.out.println("a1[1] : "+ a1[1]);
+		
+		
+		// 2. System.arrayCopy();
+		int a2[] = {1,2,3};
+		int b2[] = new int[a2.length];
+		System.arraycopy(a2,0,b2,0,a2.length);
+		// 어떤배열을 어디서부터 대상배열을 어디서부터 어디까지 복사!
+		
+		System.out.println("arrayCopy로 복사");
+		for(int x=0; x< b2.length; x++){
+			System.out.println(b2[x]);
+		}
+		
+		// Arrays.copyOf(null, 0);
+		
+		
+	}
+
+}
