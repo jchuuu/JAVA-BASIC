94f4b60bf4c33e0802258d9930e58c705c815c3e
jchuuu <chu6262j@gmail.com>
2023-05-25 21:19:17 +0900

    DAY11
    static/instance

---
+++ JAVA/src/DAY11/Block.java
@@ -0,0 +1,16 @@
+package DAY11;
+
+public class Block {
+
+	public static void main(String[] args) {
+		// static ; 프로그램이 실행될 때 한번만 할당
+		// 			모든 프로그램이 "공유"하는 영역
+		// 			정적 : class loader / garbage collector ->> heap
+		// 			프로그램이 끝날 때까지 유지
+		
+		// heap ; 
+		// stack ;
+
+	}
+
+}
